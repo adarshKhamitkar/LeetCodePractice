7db0a00e73a97a917c113adc089389cff07e7590
adarshKhamitkar <42814496+adarshKhamitkar@users.noreply.github.com>
2024-05-23 18:41:56 +0530

    Create highest_rank.sql

---
+++ SQL/highest_rank.sql
@@ -0,0 +1,58 @@
+/*
+Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key (combination of columns with unique values) of this table.
+grade is never NULL.
+ 
+
+Write a solution to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+
+Return the result table ordered by student_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+Output: 
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+
+*/
+
+select tab1.student_id,
+       tab1.course_id,
+       tab1.grade
+from (
+select student_id,
+       course_id,
+       grade,
+       row_number() over (partition by student_id order by grade desc,course_id) as rnk
+from Enrollments) tab1
+where tab1.rnk=1
