d94ad1dffa25109756eb65ac92e94ef2b0ee8554
adarshKhamitkar <42814496+adarshKhamitkar@users.noreply.github.com>
2025-07-19 11:48:01 +0530

    Create implement_queue_using_deque.py

---
+++ Python/patterns/Monotonic_Queue/implement_queue_using_deque.py
@@ -0,0 +1,42 @@
+#Implement Queue using deque
+
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.items = deque()
+        
+    def isEmpty(self) -> bool:
+        return len(self.items) == 0
+    
+    def size(self) -> int:
+        return len(self.items)
+    
+    def enqueue(self,item):
+        self.items.append(item)
+        
+    def dequeue(self):
+        if not self.isEmpty():
+            self.items.popleft()
+        else:
+            raise IndexError("List is Empty!")
+            
+    def returnQueue(self):
+        return self.items
+            
+
+if __name__ == "__main__":
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    queue.enqueue(4)
+    queue.enqueue(5)
+    queue.enqueue(6)
+    
+    print(queue.returnQueue())
+    
+    queue.dequeue()
+    
+    print(queue.returnQueue())
+  
